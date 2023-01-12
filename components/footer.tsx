1f215d547c5dddc850c30afd4da87cdea444f7d2
Tarikul Islam Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-01-12 23:44:29 +0600

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ import data from 'data/data.json'
 export default function Footer() {
   const { footer } = data
   return (
-    footer.enable && (
+    footer.display && (
       <footer className="bg-white">
         <div className="mx-auto flex max-w-7xl items-center justify-center px-4 py-6">
           <p className="text-center text-base text-gray-400">{footer.text}</p>
