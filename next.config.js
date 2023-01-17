3657f8df91de42433f7b1e352e030bf8f8d9c67b
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-01-18 00:15:17 +0600

    prettier.

--- next.config.js
+++ next.config.js
@@ -7,14 +7,14 @@ const nextConfig = {
       'pbs.twimg.com', // Twitter images
       'www.facebook.com', // Facebook images
       'instagram.fdac31-1.fna.fbcdn.net', // Instagram images
-      'm.media-amazon.com',
-    ], // Amazon images
-    typescript: {
-      ignoreBuildErrors: true,
-    },
-    experimental: {
-      fontLoaders: [{ loader: '@next/font/google', options: { subsets: ['latin'] } }],
-    },
+      'm.media-amazon.com', // Amazon images
+    ],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  experimental: {
+    fontLoaders: [{ loader: '@next/font/google', options: { subsets: ['latin'] } }],
   },
 }
 
