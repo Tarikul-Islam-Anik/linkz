e0532dc8f01db23e9e7bf972fa3880394330e9a2
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-01-17 23:56:21 +0600

    some bug fixes.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,24 @@
 import 'styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Nunito } from '@next/font/google'
-
+import Head from 'next/head'
+import data from 'data/data.json'
 const nunitoVariable = Nunito()
 
 export default function App({ Component, pageProps }: AppProps) {
+  const { name, bio, background } = data
   return (
-    <main className={`${nunitoVariable.className}`}>
-      <Component {...pageProps} />
-    </main>
+    <>
+      <Head>
+        <title>{name}</title>
+        <meta name="description" content={bio} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content={background} />
+        <link rel="icon" href="/favicon.webp" />
+      </Head>
+      <main className={`${nunitoVariable.className}`}>
+        <Component {...pageProps} />
+      </main>
+    </>
   )
 }
