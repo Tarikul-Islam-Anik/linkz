a5283615854cb9ea8e6cc3b1a4d367066ed74d91
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-01-17 23:55:00 +0600

    added new infos plus design adjustments.

--- components/user_info.tsx
+++ components/user_info.tsx
@@ -1,23 +1,35 @@
-import Image from 'next/legacy/image'
 import data from 'data/data.json'
 import Icon from './icons'
+import Button from './button'
+import UserImages from './user_images'
 export default function UserInfo() {
+  const { name, images, bio, buttons, location, website } = data
   return (
-    <div className="relative flex flex-col items-center justify-center space-y-2 rounded-2xl bg-white p-8">
-      <Image src={'/' + data.image} alt="OxyZen" width={96} height={96} className="rounded-full" />
-      <div className="text-center">
-        <h1 className="text-2xl font-bold leading-loose text-stone-800">{data.name}</h1>
-        <p className="font-medium text-stone-500">{data.bio}</p>
+    <div className="flex flex-col items-center justify-center space-y-6 rounded-2xl bg-white px-4 pt-4 pb-6">
+      <div className="mb-12">
+        <UserImages images={images} alt={name} />
       </div>
-      <a
-        href={'https://' + data.website}
-        target="_blank"
-        rel="noreferrer"
-        className="flex items-center justify-center tracking-wide text-stone-400 transition hover:text-stone-500"
-      >
-        {data.website}
-        <Icon label="externallink" />
-      </a>
+      <div className="px-4 text-center">
+        <h1 className="mb-3 flex flex-col text-2xl font-bold text-stone-800">
+          {name}
+          {location && <span className="text-sm font-medium text-gray-400">{location}</span>}
+        </h1>
+        <p className="font-medium text-stone-500">{bio}</p>
+      </div>
+      <div className="flex space-x-3">
+        {buttons.display && buttons.items.map((item, index) => <Button key={index} item={item} />)}
+      </div>
+      {website && (
+        <a
+          href={'https://' + website}
+          target="_blank"
+          rel="noreferrer"
+          className="flex items-center justify-center tracking-wide text-stone-400 transition hover:text-stone-500"
+        >
+          {website}
+          <Icon label="externallink" />
+        </a>
+      )}
     </div>
   )
 }
