ea760e823cb749bea9974984183a8b125b25c67f
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-01-18 00:07:54 +0600

    fixed a bug.

--- next.config.js
+++ next.config.js
@@ -2,25 +2,19 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-<<<<<<< HEAD
   images: {
     domains: [
       'pbs.twimg.com', // Twitter images
       'www.facebook.com', // Facebook images
       'instagram.fdac31-1.fna.fbcdn.net', // Instagram images
-      'm.media-amazon.com', // Amazon images
-=======
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  experimental: {
-    fontLoaders: [
-      { loader: "@next/font/google", options: { subsets: ["latin"] } },
->>>>>>> 7d8cc9fd9f3238f5aec871b37af5c27e91806913
-    ],
-  },
-  experimental: {
-    fontLoaders: [{ loader: '@next/font/google', options: { subsets: ['latin'] } }],
+      'm.media-amazon.com',
+    ], // Amazon images
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+    experimental: {
+      fontLoaders: [{ loader: '@next/font/google', options: { subsets: ['latin'] } }],
+    },
   },
 }
 
