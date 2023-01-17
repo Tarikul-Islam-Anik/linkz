cb2bcdab08db6bb7f1f0ab1c4b373b95a590659a
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-01-17 23:56:29 +0600

    added new sections.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,19 @@
 import { UserInfo, Section } from 'components'
 import Profile from './../layouts/profile'
 import data from 'data/data.json'
+import QNA from 'components/qna'
 
 export default function Home() {
-  const { contacts, socials } = data
+  const { contacts, socials, designs, gaming_profiles, wallets, qna } = data
   return (
     <Profile>
       <UserInfo />
       {contacts.display && <Section name="Contacts" items={contacts.items} />}
       {socials.display && <Section name="Socials" items={socials.items} />}
+      {designs.display && <Section name="Designs" items={designs.items} />}
+      {gaming_profiles.display && <Section name="Gaming" items={gaming_profiles.items} />}
+      {wallets.display && <Section name="Wallets" items={wallets.items} />}
+      {qna.display && <QNA {...qna} />}
     </Profile>
   )
 }
